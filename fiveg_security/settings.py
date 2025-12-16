c63cb02927200af1c6d058528e695597682b3f63
Mike Ouma <oumamikewab@gmail.com>
2025-12-16 03:07:41 +0300

    Added hosts for Render deployment

--- fiveg_security/settings.py
+++ fiveg_security/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6@=5ij6&@*vel++@%h6q(!cmnaz=9$f2o2l1r(=17s^tc=b$7z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fiveg-zero-trust-sumulation-3.onrender.com']
 
 
 # Application definition
